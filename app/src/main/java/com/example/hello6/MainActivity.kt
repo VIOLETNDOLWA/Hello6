77920c21b0d8942a8784bbeef1ea73947d77ef05
VIOLETNDOLWA <violetndolwa@gmail.com>
2020-08-04 05:59:30 +0300

    my commit

--- app/src/main/java/com/example/hello6/MainActivity.kt
+++ app/src/main/java/com/example/hello6/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.hello6
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.widget.EditText
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val Textview =findViewById<TextView>(Id.nameTextview)
+        val Textview =findViewById<TextView>(R.Id.nameTextview)
+        val EditTexview =findViewById<EditText>(R.Id.EditTextview)
+        val EditText =findViewById<EditText>(R.id.EditText)
     }
 }
